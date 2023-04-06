86802b32cb12dd988bb3b62c3edd4b0b7136e75b
Felixdiamond <diamondfelix006@gmail.com>
2023-04-06 12:04:07 -0700

    Anya

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,35 +1,50 @@
 #!/usr/bin/python3
-#  Fabric script that generates a .tgz archive from the
-# contents of the web_static folder of your AirBnB Clone repo
-# using the function do_pack
-import os
-from fabric.api import run, put, env
+"""Fabric script that generates a .tgz archive from the contents of
+the web_static folder of your AirBnB Clone repo, distributes an archive
+to your web servers, and deploys it on your servers.
+"""
 
-env.hosts = ['100.26.173.94', '35.153.79.52']
-env.user = "ubuntu"
+import os.path
+from fabric.api import env, put, run
+from fabric.operations import sudo
+
+
+env.hosts = ['<IP web-01>', '<IP web-02>']
 
 
 def do_deploy(archive_path):
-    """Create a tar gzipped archive of the directory web_static."""
-    if os.path.exists(archive_path) is False:
+    """Distributes an archive to your web servers"""
+    if not os.path.exists(archive_path):
+        return False
+    try:
+        # Upload the archive to the /tmp/ directory of the web server
+        put(archive_path, '/tmp/')
+
+        # Uncompress the archive to the folder
+        archive_name = os.path.basename(archive_path)
+        folder_name = '/data/web_static/releases/' + \
+            archive_name.split('.')[0]
+        run('sudo mkdir -p {}'.format(folder_name))
+        run('sudo tar -xzf /tmp/{} -C {}'.
+            format(archive_name, folder_name))
+
+        # Delete the archive from the web server
+        run('sudo rm /tmp/{}'.format(archive_name))
+
+        # Move contents of the folder into folder_name
+        run('sudo mv {}/web_static/* {}'.format(folder_name, folder_name))
+
+        # Remove web_static folder
+        run('sudo rm -rf {}/web_static'.format(folder_name))
+
+        # Delete the symbolic link
+        run('sudo rm -rf /data/web_static/current')
+
+        # Create new symbolic link
+        run('sudo ln -s {} /data/web_static/current'.
+            format(folder_name))
+    except:
         return False
     else:
-        try:
-            put(archive_path, "/tmp/")
-            """ putting the file to .tgz """
-            file_name = archive_path.split("/")[1]
-            """ splitting .tgz """
-            file_name2 = file_name.split(".")[0]
-            """ spliting archivo """
-            final_name = "/data/web_static/releases/" + file_name2 + "/"
-            run("mkdir -p " + final_name)
-            run("tar -xzf /tmp/" + file_name + " -C " + final_name)
-            run("rm /tmp/" + file_name)
-            run("mv " + final_name + "web_static/* " + final_name)
-            run("rm -rf " + final_name + "web_static")
-            run("rm -rf /data/web_static/current")
-            run("ln -s " + final_name + " /data/web_static/current")
-            print("New version deployed!")
-            return True
-        except Exception:
-            return False
+        return True
+
